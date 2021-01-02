0ee38e9f888c2458caea440cfcaf129f1369a3e0
Henry Wilson <henryrwilson1@gmail.com>
2021-01-02 15:32:32 +0000

    Update msvcmake.bat

--- msvcmake.bat
+++ msvcmake.bat
@@ -1,6 +1,3 @@
-cls
-:: Compile to obj file
+
 cl /c /O1 /GS- /GL GOL.c
-:: Link with no static libraries
 link /NODEFAULTLIB /ENTRY:main /SUBSYSTEM:CONSOLE /ALIGN:16 /LTCG GOL.obj ucrt.lib
-::Crinkler.exe /NODEFAULTLIB /ENTRY:main /SUBSYSTEM:CONSOLE /ALIGN:32 GOL.obj ucrt.lib user32.lib
